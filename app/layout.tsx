43c3a7dff4582d19197fe4cec3a7ada69f6c7e6f
Triehuynh123 <huynh3896@gmail.com>
2024-07-30 18:44:08 +0700

    socket

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import {
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,8 +35,10 @@ export default function RootLayout({
             enableColorScheme={false}
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
