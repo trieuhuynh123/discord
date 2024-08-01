f32994590a487f837aff0718e54205777dcc35a9
Triehuynh123 <huynh3896@gmail.com>
2024-08-01 16:58:07 +0700

    chatItem

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,7 +39,7 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>{children}</QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
--- pages/api/socket/messages/index.ts
+++ pages/api/socket/messages/index.ts
@@ -73,7 +73,7 @@ export default async function handler(
         },
       },
     });
-    const channelKey = `chat:${channelId}: messages`;
+    const channelKey = `chat:${channelId}:messages`;
     res?.socket?.server?.io?.emit(channelKey, message);
     return res.status(200).json(message);
   } catch (error) {
