10fd6400256e5ef6211f257aca2676fe7e5deaa5
Triehuynh123 <huynh3896@gmail.com>
2024-08-02 19:40:26 +0700

    fix sign up

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,16 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/api/uploadthing"])
+const isPublicRoute = createRouteMatcher(["/api/uploadthing"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) return
-})
+  if (isPublicRoute(req)) return;
+});
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
